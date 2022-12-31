84d471e46e9c12bf26f0dfb618778079ff9c736e
Dima <Uchiha132132qqq@gmail.com>
2022-12-31 11:57:32 +0300

    add tabs

--- src/components/TaskColumn/TaskColumn.jsx
+++ src/components/TaskColumn/TaskColumn.jsx
@@ -3,7 +3,9 @@ import { Task } from "../Task/Task";
 import styles from "./TaskColumn.module.css";
 
 export const TaskColumn = React.forwardRef((props, ref) => {
+
   const { title, tasks, children, footer } = props;
+  
   return (
     <div className={styles.tasks__column} ref={ref}>
       <h3 className={styles.tasks__taskName}>{title}</h3>
