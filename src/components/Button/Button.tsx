a43e1b6f6f04b600d099a2b0f6d41450e7577dcc
Dima <Uchiha132132qqq@gmail.com>
2023-01-01 12:27:49 +0300

    add styles logic for Button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,15 +1,28 @@
-import React, { FC } from 'react'
-import styles from './Button.module.css';
+import React, { FC } from "react";
+import styles from "./Button.module.css";
 
 interface IButton {
   onClick: VoidFunction;
   title: string;
+  color: string;
 }
 
-export const Button: FC<IButton> = ({onClick, title}) => {
+export const Button: FC<IButton> = ({ onClick, title, color }) => {
+  let bgColor;
+  if (color === "red") {
+    bgColor = styles.button_red;
+  } else if (color === "purple") {
+    bgColor = styles.button_purple;
+  } else if (color === "yellow") {
+    bgColor = styles.button_yellow;
+  } else if (color === "green") {
+    bgColor = styles.button_green;
+  } else if (color === "orange") {
+    bgColor = styles.button_orange;
+  }
   return (
-    <button onClick={onClick} className={styles.button}  >
+    <button onClick={onClick} className={`${styles.button} ${bgColor}`}>
       {title}
     </button>
-  )
-}
+  );
+};
