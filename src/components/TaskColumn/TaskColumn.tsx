e56a0784c7f05be1114cf1f43308153f9c997ee7
Dima <Uchiha132132qqq@gmail.com>
2023-01-07 13:01:39 +0300

    taskColumn ts

--- src/components/TaskColumn/TaskColumn.tsx
+++ src/components/TaskColumn/TaskColumn.tsx
@@ -1,15 +1,20 @@
 import React, { FC } from "react";
+import { ConnectDropTarget } from "react-dnd";
+import { ITask } from "../../services/types/data";
 import { Task } from "../Task/Task";
 import styles from "./TaskColumn.module.css";
 
 interface ITaskColumn {
-  
+  ref: ConnectDropTarget;
+  title: string;
+  tasks: ITask[];
+  children: React.ReactNode;
+  footer: any;
 }
 
-export const TaskColumn: FC = React.forwardRef((props, ref) => {
-
+export const TaskColumn: FC<ITaskColumn> = React.forwardRef((props, ref) => {
   const { title, tasks, children, footer } = props;
-  
+
   return (
     <div className={styles.tasks__column} ref={ref}>
       <h3 className={styles.tasks__taskName}>{title}</h3>
