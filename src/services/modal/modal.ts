4c1416c09999bdbabb913b4c81cc439d739ff193
Dima <Uchiha132132qqq@gmail.com>
2023-01-07 12:09:23 +0300

    add ts for modal reducer

--- src/services/modal/modal.ts
+++ src/services/modal/modal.ts
@@ -1,10 +1,5 @@
 import { IBoards, ITask } from "../types/data";
-
-export const OPEN_MODAL = "OPEN_MODAL";
-export const CLOSE_MODAL = "CLOSE_MODAL";
-export const SET_CURRENT_TASK = "SET_CURRENT_TASK";
-export const SET_CURRENT_BOARD = "SET_CURRENT_BOARD";
-
+import { TModalActions, ModalActionTypes } from "./actions";
 
 interface IModalState {
   active: boolean;
@@ -18,41 +13,29 @@ const initialState: IModalState = {
   currentBoard: null,
 };
 
-export const modal = (state = initialState, action): IModalState => {
+export const modal = (
+  state = initialState,
+  action: TModalActions
+): IModalState => {
   switch (action.type) {
-    case OPEN_MODAL:
+    case ModalActionTypes.OPEN_MODAL:
       return {
+        ...state,
         active: true,
       };
-    case CLOSE_MODAL:
+    case ModalActionTypes.CLOSE_MODAL:
       return {
+        ...state,
         active: false,
       };
-    case SET_CURRENT_TASK:
+    case ModalActionTypes.SET_CURRENT_TASK:
       return {
         ...state,
         currentTask: action.payload,
       };
-    case SET_CURRENT_BOARD:
+    case ModalActionTypes.SET_CURRENT_BOARD:
       return { ...state, currentBoard: action.payload };
     default:
       return state;
   }
 };
-
-export const openModal = () => ({
-  type: OPEN_MODAL,
-});
-
-export const closeModalAction = () => ({
-  type: CLOSE_MODAL,
-});
-
-export const setCurrentTask = (payload) => ({
-  type: SET_CURRENT_TASK,
-  payload,
-});
-export const setCurrentBoard = (payload) => ({
-  type: SET_CURRENT_BOARD,
-  payload,
-});
